156db3d6169e9994f62e42e334d433a5d28e3546
wang-jie-2020 <421004034@qq.com>
2021-12-22 16:14:39 +0800

    init

---
+++ src/_infrastructure/Hugin.Infrastructure.Log.Serilog/Infrastructure/Log/Serilog/Enriches/ThreadIdEnricher.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Threading;
+
+namespace Hugin.Infrastructure.Log.Serilog.Enriches
+{
+    public class ThreadIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+
+    public static class ThreadIdEnricherExtensions
+    {
+        public static LoggerConfiguration WithThreadId(this LoggerEnrichmentConfiguration enrich)
+        {
+            if (enrich == null)
+            {
+                throw new ArgumentNullException(nameof(enrich));
+            }
+
+            return enrich.With<ThreadIdEnricher>();
+        }
+    }
+}
+
+
+
---
+++ src/_infrastructure/Hugin.Infrastructure.Log.Serilog/Infrastructure/Log/Serilog/Filters/LogApiAttributeFilter.cs
@@ -0,0 +1,14 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Serilog.Filters;
+
+namespace Hugin.Infrastructure.Log.Serilog.Filters
+{
+    public class LogApiAttributeFilter : ILogEventFilter
+    {
+        public bool IsEnabled(LogEvent logEvent)
+        {
+            return Matching.FromSource<LogApiAttribute>()(logEvent);
+        }
+    }
+}
